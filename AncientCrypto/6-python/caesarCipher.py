91f3c66f9c13547a343eb0364c0ae10fa874c903
jordi-fontan <63612112+jordi-fontan@users.noreply.github.com>
2022-11-29 17:09:00 +0100

    add caesars python

--- AncientCrypto/6-python/caesarCipher.py
+++ AncientCrypto/6-python/caesarCipher.py
@@ -8,9 +8,58 @@
   
   # Read ipnut
 
-# Build shifted alphabet
+  
 
-# Encript
+def shiftAlphabet(k):
+  # Build shifted alphabet
+  lalphabet="abcdefghijklmnopqrstuvwxyz"
+  ualphabet=lalphabet.upper()	
+
+  shifted = lalphabet[k:] + lalphabet[0:k]
+  #print(shifted)
+  return shifted
+
 
 
 # Show output
+def encryptCaesar(key, message):
+  enc=[]
+# Encript
+  for currChar in message:
+    shiftAlpha=shiftAlphabet(key)
+    print(currChar)
+    if currChar in shiftAlpha:
+      idx=shiftAlpha.index(currChar.lower())     
+    else:
+      continue
+    newChar=shiftAlpha[idx]     
+    if currChar.isupper(): 
+      newChar.upper
+  print(currChar,newChar)
+  enc.append(newChar)
+  print(enc)
+  return enc	
+
+  
+
+
+
+
+
+test= "At noon be in the conference room with your hat on for a surprise party. YELL LOUD!";
+k1=13
+k2=21
+mid=encryptCaesar(k1,test)
+final=encryptCaesar(k2,mid)  
+
+
+## "I want {} pieces of item {} for {} dollars."
+print(final)
+
+#print("First Encryption with key " + str(k1) + " : " +mid)
+#print("Second Encryption (over already Encrypted Message ) with key " + str(k2) + " : " +final);
+
+
+
+
+
