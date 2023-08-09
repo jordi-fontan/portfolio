806724520186f6044c7c299afc642832c68cbfe8
jordi-fontan <63612112+jordi-fontan@users.noreply.github.com>
2023-08-09 10:41:20 +0200

    add inner class Car annotation idea

---
+++ JavaBackEnd/DemoCollections/src/main/java/generics/General.java
@@ -0,0 +1,39 @@
+package generics;
+
+public class General {
+
+	class RaceCar{
+		private static final String Logs = "Logs";
+		private String name;
+		private float speed;
+		
+		@Override
+		public String toString() {
+			return "RaceCar [name=" + name + ", speed=" + speed + "]";
+		}
+		public String getName() {
+			return name;
+		}
+		public void setName(String name) {
+			this.name = name;
+		}
+		public float getSpeed() {
+			return speed;
+		}
+		public void setSpeed(float speed) {
+			this.speed = speed;
+		}
+		@Idea(Logs)
+		public void race()
+		{
+			
+		}
+	}
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ JavaBackEnd/DemoCollections/src/main/java/generics/Idea.java
@@ -0,0 +1,14 @@
+package generics;
+
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.SOURCE;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+@Retention(SOURCE)
+@Target({ TYPE, METHOD })
+public @interface Idea {
+
+}
